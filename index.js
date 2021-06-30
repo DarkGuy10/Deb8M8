efeffc06ac423e11c9eeca32fe741ac1d6cd1461
Dallas <78465594+DallasOnBackup@users.noreply.github.com>
2021-06-29 23:41:59 -0500

    shortened code

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const express = require('express')
-const app = express()
-const http = require('http')
-const server = http.createServer(app)
+const app = require('express')()
+const server = require('http').Server(app)
 const { Server } = require('socket.io')
 const io = new Server(server)
 
