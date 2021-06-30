82d19fad76898773465edc7dc0bf9f7a3988ce38
DarkGuy10 <HarperBurn@protonmail.ch>
2021-06-30 10:43:35 +0530

    Fixed index.html not found

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+const path = require('path')
 const app = require('express')()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+  res.sendFile(path.join(__dirname, 'Assets/index.html'))
 })
 
 server.listen(3000, () => {
