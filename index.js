c9ac3b0ce691a51ca1096bd8922c7bf5fa94fed2
Dallas <78465594+DallasOnBackup@users.noreply.github.com>
2021-06-30 00:18:59 -0500

    debate creation/search

--- index.js
+++ index.js
@@ -10,3 +10,20 @@ app.get('/', (req, res) => {
 server.listen(3000, () => {
   console.log('Server Started on :3000!')
 })
+
+io.on('connection', (socket) => {
+	socket.emit("connection")
+	
+	socket.on("requestDebates", (name) => {
+		socket.send(Object.assign({
+			event: "requestDebates"
+		}, db.get(`debates.${encodeURI(name)}`)))
+	})
+	
+	socket.on("createDebate", (data) => {
+		db.push('debates', data)
+		socket.send(Object.assign({
+			event: "createDebate"
+		}, db.get(`debates.${encodeURI(data.name)}`))
+	})
+})
